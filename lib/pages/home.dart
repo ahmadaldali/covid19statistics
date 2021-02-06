06f452f12f6102d45db6e93fb9ccc06fdc7cef4f
ahmad_aldali <ahmad@zeuor.com>
2021-02-06 09:36:28 +0200

    remove conncction

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,11 +3,8 @@ import 'package:covid19/pages/country_statistics.dart';
 import 'package:covid19/pages/summary.dart';
 import 'package:covid19/provider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
-import 'dart:async';
-import 'package:connectivity/connectivity.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -17,58 +14,12 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int _currentIndex = 0;
 
-  String _connectionStatus = 'Unknown';
-  final Connectivity _connectivity = Connectivity();
-  StreamSubscription<ConnectivityResult> _connectivitySubscription;
-
   @override
   void initState() {
     Provider.of<SummaryProvider>(context, listen: false).getSummaryApi();
     Provider.of<SummaryProvider>(context, listen: false).getCountriesApi();
     Provider.of<SummaryProvider>(context, listen: false).getAllCountriesApi();
     super.initState();
-    initConnectivity();
-    _connectivitySubscription =
-        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
-  }
-
-  @override
-  void dispose() {
-    _connectivitySubscription.cancel();
-    super.dispose();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initConnectivity() async {
-    ConnectivityResult result;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      result = await _connectivity.checkConnectivity();
-    } on PlatformException catch (e) {
-      print(e.toString());
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) {
-      return Future.value(null);
-    }
-
-    return _updateConnectionStatus(result);
-  }
-
-  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
-    switch (result) {
-      case ConnectivityResult.wifi:
-      case ConnectivityResult.mobile:
-      case ConnectivityResult.none:
-        setState(() => _connectionStatus = result.toString());
-        break;
-      default:
-        setState(() => _connectionStatus = 'Failed to get connectivity.');
-        break;
-    }
   }
 
   void _setIndex(int newVal) {
@@ -105,10 +56,9 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      /*   body: _currentIndex == 0
+      body: _currentIndex == 0
           ? Summary()
-          : (_currentIndex == 1 ? Countries() : CountryStatistics()), */
-      body: Center(child: Text('Connection Status: $_connectionStatus')),
+          : (_currentIndex == 1 ? Countries() : CountryStatistics()),
     );
   }
 }
